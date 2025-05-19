b5278cc0145545bc6a22b5e56ce6930675712ed0
Brandon Corrigall <corrb812@gmail.com>
2025-05-19 14:43:35 -0400

    created index.js and created a simple fahrenheit to celcius converter

---
+++ index.js
@@ -0,0 +1,29 @@
+const convertToCelcius = (fahrenheit) => {
+  const celcius = ((fahrenheit - 32) * 5) / 9;
+  return celcius;
+};
+
+const fahrenheit = Number(prompt(`Enter a tempurature in Fahrenheit`));
+
+const describeTempurature = (fahrenheit) => {
+  const convertedFahrenheit = convertToCelcius(fahrenheit);
+  if (convertedFahrenheit < 0) {
+    return `${fahrenheit} degrees fahrenheit is ${convertedFahrenheit} degrees celcius. 
+    That would be very cold. Time to get the snow shovels.`;
+  } else if (convertedFahrenheit < 20) {
+    return `${fahrenheit} degrees fahre>nheit is ${convertedFahrenheit} degrees celcius. 
+    That would be moderately cold. Bundle up, but be ready to take some layers off.`;
+  } else if (convertedFahrenheit < 30) {
+    return `${fahrenheit} degrees fahrenheit is ${convertedFahrenheit} degrees celcius. 
+    That would be slightly warm. This, this is perfect.`;
+  } else if (convertedFahrenheit < 40) {
+    return `${fahrenheit} degrees fahrenheit is ${convertedFahrenheit} degrees celcius. 
+    That would be hot. Probably a good beach day.`;
+  } else if (convertedFahrenheit >= 40) {
+    return `${fahrenheit} degrees fahrenheit is ${convertedFahrenheit} degrees celcius. 
+    That would be very hot. Maybe stay inside for a while.`;
+  }
+};
+
+const description = describeTempurature(fahrenheit);
+alert(description);
